c41418a568f20769d3d40f1dc8646aad415a9564
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-06-27 18:59:01 -0300

    Added @CrossOrigin

--- src/main/java/br/edu/ifg/pw/storecluster/dao/ProductCategoryRepository.java
+++ src/main/java/br/edu/ifg/pw/storecluster/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package br.edu.ifg.pw.storecluster.dao;
 import br.edu.ifg.pw.storecluster.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- src/main/java/br/edu/ifg/pw/storecluster/dao/ProductRepository.java
+++ src/main/java/br/edu/ifg/pw/storecluster/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package br.edu.ifg.pw.storecluster.dao;
 
 import br.edu.ifg.pw.storecluster.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
